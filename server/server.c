6a17e0a49681b5cbc7e82c776bc6f0145c8631ca
rhit-amatoam <amatoam@rose-hulman.edu>
2023-05-04 15:23:17 -0400

    adding on folder access

--- server/server.c
+++ server/server.c
@@ -13,8 +13,13 @@
 #include <ctype.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/sendfile.h>
 
 #define BACKLOG 10 //how many pending connections queue will hold
+
+void accessFolder(int new_fd, char* foldername);
+void downloadFolder(int new_fd, char* foldername);
 
 void sigchild_handler(int s) {
     (void)s; //quiet unused vriable warning
@@ -187,26 +192,29 @@ int main(int argc, char *argv[]) {
                 }
 
                 if(strcmp(cmd, "access") == 0) {
-                    // Step 3a. 
+                    // Step 3a. HAS NOT BEEN TESTED
+                    accessFolder(new_fd, foldername);
                 }
-                if(strcmp(cmd, "download") == 0) {
-                    // Step 3b. 
+                else if(strcmp(cmd, "download") == 0) {
+                    // Step 3b. HAS NOT BEEN TESTED
+                    downloadFolder(new_fd, foldername);
                 }
-                if(strcmp(cmd, "create") == 0) {
+                else if(strcmp(cmd, "create") == 0) {
                     // Step 3c.
                     if (mkdir(foldername, 0777) != 0) {
-                        printf("Error creating directory!\n\n");
-                        return 1;
+                        printf("Error: could not create folder '%s'\n\n", foldername);
                     }
-                    printf("Directory created successfully.\n\n");
+                    printf("Folder '%s' created successfully.\n\n", foldername);
                 }
-                if(strcmp(cmd, "delete") == 0) {
+                else if(strcmp(cmd, "delete") == 0) {
                     // Step 3d. 
                     if (rmdir(foldername) != 0) {
-                        printf("Error: could not delete folder '%s'\n", foldername);
+                        printf("Error: could not delete folder '%s'\n\n", foldername);
                     } else {
-                        printf("Folder '%s' deleted successfully\n", foldername);
+                        printf("Folder '%s' deleted successfully\n\n", foldername);
                     }
+                } else {
+                    printf("Please use commands: access, download, create, and delete.");
                 }
             }
             printf("\tClient finished, now waiting to service another client...\n\n");
@@ -218,4 +226,127 @@ int main(int argc, char *argv[]) {
     }
 
     return 0;
+}
+
+void accessFolder(int new_fd, char* foldername) {
+    char file_names[1024] = "";
+    struct dirent *de;
+    DIR *dr = opendir(foldername);
+    if (dr == NULL) {
+        printf("Could not open current directory\n\n" );
+        exit(1);
+    }
+    while ((de = readdir(dr)) != NULL) {
+        if (de->d_type == DT_REG && strcmp(de->d_name, ".") != 0 && strcmp(de->d_name, "..") != 0) {
+            strcat(file_names, de->d_name);
+            strcat(file_names, ",");
+        }
+    }
+    closedir(dr);
+    printf("Files in %s: %s\n\n", foldername, file_names);
+    if(send(new_fd, file_names, strlen(file_names), 0) == -1) {
+        perror("send");
+    }
+
+    printf("\tNow listening for incoming messages...\n\n");
+    char buf[1024];
+    int numbytes;
+    while(1) {
+        // Step 3. The server receives/parses either of the following messages:
+        //  “access foldername”, “download foldername”, “create foldername”, “delete foldername”
+        if((numbytes = recv(new_fd, buf, 99, 0)) == -1) {
+            perror("recv");
+            exit(1);
+        }
+        buf[numbytes] = '\0';
+        printf("\tReceived the following message from client:\n\n");
+        printf("\t\t\"%s\"\n\n", buf);
+
+        if(strlen(buf) == 0 || strcmp(buf, "exit") == 0) break;
+
+
+        char cmd[15];
+        char filename[256];
+        int j = 0;
+        int atFileName = 0;
+        for(int i = 0; i < strlen(buf); i++) {
+            if(buf[i] == ' ') {
+                atFileName = 1;
+                cmd[i] = '\0';
+            }
+            else if(atFileName == 1) {
+                filename[j] = buf[i];
+                j++;
+            }
+            else cmd[i] = buf[i];
+            
+        }
+        filename[j] = '\0';
+
+        printf("cmd: %s\n", cmd);
+        printf("folder: %s\n", filename);
+
+        printf("\tNow sending parsed string back...\n\n");
+        if(send(new_fd, cmd, strlen(cmd), 0) == -1) {
+            perror("send");
+        }
+
+        if(strcmp(cmd, "access") == 0) {
+            // Step 3a. 
+        }
+        else if(strcmp(cmd, "download") == 0) {
+            // Step 3b. 
+        }
+        else if(strcmp(cmd, "upload") == 0) {
+            // Step 3c. 
+        }
+        else if(strcmp(cmd, "delete") == 0) {
+            // Step 3d. 
+            char path[256] = "/";
+            strcat(path, foldername);
+            strcat(path, "/");
+            strcat(path, filename);
+            if(remove(path) != 0) {
+                printf("Error deleting file: '%s'\n\n", filename);
+            } else {
+                printf("File '%s' deleted successfully\n\n", filename);
+            }
+        } else {
+            printf("Please use commands: access, download, upload, and delete.");
+        }
+    }
+    return;
+}
+
+void downloadFolder(int new_fd, char* foldername) {
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir(foldername)) == NULL) {
+        perror("opendir failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Read the folder and send each file to the client
+    while ((ent = readdir(dir)) != NULL) {
+        if (ent->d_name[0] == '.') {
+            // Skip hidden files
+            continue;
+        }
+        char filepath[1024];
+        snprintf(filepath, sizeof(filepath), "%s/%s", foldername, ent->d_name);
+        int fd = open(filepath, O_RDONLY);
+        if (fd < 0) {
+            perror("open failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Send the file to the client
+        off_t offset = 0;
+        struct stat file_stat;
+        fstat(fd, &file_stat);
+        sendfile(new_fd, fd, &offset, file_stat.st_size);
+
+        close(fd);
+    }
+    return;
 }
