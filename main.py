822e68c1637bd222f8144b35a965ffc7fa249f02
MagdelineNg <MNG045@e.ntu.edu.sg>
2023-05-10 23:26:43 -0700

    Added folder to file screen

--- main.py
+++ main.py
@@ -12,6 +12,43 @@ root.geometry("450x560+500+200")
 root.configure(bg="#f4fdfe")
 root.resizable(False,False)
 
+def receive_option():
+    # Load and resize receive icons
+    receive_img = Image.open("./assets/icons/receive.png")
+
+    #Update bg color of icon - Composite the original image on top of the new image
+    new_receive_image = Image.new("RGBA", receive_img.size, "#f4fdfe")
+    new_receive_image.paste(receive_img, (0, 0), receive_img)
+
+    new_receive_image = new_receive_image.resize((50, 50), Image.ANTIALIAS)
+    new_receive_image = ImageTk.PhotoImage(new_receive_image)
+    return new_receive_image
+
+
+def delete_option():
+    # Load and resize delete icons
+    delete_img = Image.open("./assets/icons/delete.png")
+
+    #Update bg color of icon - Composite the original image on top of the new image
+    new_delete_image = Image.new("RGBA", delete_img.size, "#f4fdfe")
+    new_delete_image.paste(delete_img, (0, 0), delete_img)
+
+    new_delete_image = new_delete_image.resize((50, 50), Image.ANTIALIAS)
+    new_delete_image = ImageTk.PhotoImage(new_delete_image)
+    return new_delete_image
+    #delete_btn.grid(row=0, column=2, sticky=tkinter.E)
+
+def upload_option(window, text):
+    bottom_frame = LabelFrame(window)
+    bottom_frame.pack(side=tkinter.BOTTOM, expand=False, fill=BOTH)
+
+    #button to add new folders
+    create_folder_icon = Button(bottom_frame, text=text,font='arial 14 bold',bg="#f4fdfe")
+    # create_folder_icon.pack(side=tkinter.BOTTOM)
+    create_folder_icon.pack(side=tkinter.RIGHT, expand=False, fill="y")
+   
+    window.mainloop() 
+
 def display_files(folder_name):
     window = Toplevel(root)
     window.title("All files")
@@ -26,6 +63,28 @@ def display_files(folder_name):
     #header_title.grid(row=0, column=0, columnspan=3)
     header_title.pack(side=tkinter.TOP, expand=True, fill=BOTH)
 
+    #file labelframe
+    file_frame = LabelFrame(top_frame,background="#f4fdfe")
+    file_frame.pack(expand=False, fill=BOTH)
+
+    #display all folders
+    file1 = Label(file_frame, text="file.txt", background="#f4fdfe", cursor="arrow", anchor=tkinter.W)
+    # Bind the label to a click event
+    file1.bind("<Button-1>", folder_clicked)
+    file1.pack(side=tkinter.LEFT)
+
+    #receive_delete_option(file_frame)
+    new_rcv_img = receive_option()
+    receive_btn = tkinter.Button(file_frame, image=new_rcv_img, anchor=tkinter.E)
+    receive_btn.configure(borderwidth=0, highlightthickness=0, relief="flat")
+    receive_btn.pack(side=tkinter.RIGHT)
+    #receive_btn.grid(row=0, column=1, sticky=tkinter.E)
+    new_del_img = delete_option()
+    receive_btn = tkinter.Button(file_frame, image=new_del_img, anchor=tkinter.E)
+    receive_btn.configure(borderwidth=0, highlightthickness=0, relief="flat")
+    receive_btn.pack(side=tkinter.RIGHT)
+
+    upload_option(window, "Upload file")
 
 def folder_clicked(event):
     print("Folder clicked")
@@ -61,43 +120,18 @@ def Send():
     folder1.bind("<Button-1>", folder_clicked)
     folder1.pack(side=tkinter.LEFT)
 
-    # Load and resize receive icons
-    receive_img = Image.open("./assets/icons/receive.png")
-
-    #Update bg color of icon - Composite the original image on top of the new image
-    new_receive_image = Image.new("RGBA", receive_img.size, "#f4fdfe")
-    new_receive_image.paste(receive_img, (0, 0), receive_img)
-
-    new_receive_image = new_receive_image.resize((50, 50), Image.ANTIALIAS)
-    new_receive_image = ImageTk.PhotoImage(new_receive_image)
-    receive_btn = tkinter.Button(file_frame, image=new_receive_image, anchor=tkinter.E)
+    #receive_delete_option(file_frame)
+    new_rcv_img = receive_option()
+    receive_btn = tkinter.Button(file_frame, image=new_rcv_img, anchor=tkinter.E)
     receive_btn.configure(borderwidth=0, highlightthickness=0, relief="flat")
     receive_btn.pack(side=tkinter.RIGHT)
     #receive_btn.grid(row=0, column=1, sticky=tkinter.E)
+    new_del_img = delete_option()
+    receive_btn = tkinter.Button(file_frame, image=new_del_img, anchor=tkinter.E)
+    receive_btn.configure(borderwidth=0, highlightthickness=0, relief="flat")
+    receive_btn.pack(side=tkinter.RIGHT)
 
-    # Load and resize delete icons
-    delete_img = Image.open("./assets/icons/delete.png")
-
-    #Update bg color of icon - Composite the original image on top of the new image
-    new_delete_image = Image.new("RGBA", delete_img.size, "#f4fdfe")
-    new_delete_image.paste(delete_img, (0, 0), delete_img)
-
-    new_delete_image = new_delete_image.resize((50, 50), Image.ANTIALIAS)
-    new_delete_image = ImageTk.PhotoImage(new_delete_image)
-    delete_btn = tkinter.Button(file_frame, image=new_delete_image, anchor=tkinter.E)
-    delete_btn.configure(borderwidth=0, highlightthickness=0, relief="flat")
-    delete_btn.pack(side=tkinter.RIGHT)
-    #delete_btn.grid(row=0, column=2, sticky=tkinter.E)
-
-    bottom_frame = LabelFrame(window)
-    bottom_frame.pack(side=tkinter.BOTTOM, expand=False, fill=BOTH)
-
-    #button to add new folders
-    create_folder_icon = Button(bottom_frame, text="Create folder",font='arial 14 bold',bg="#f4fdfe")
-    # create_folder_icon.pack(side=tkinter.BOTTOM)
-    create_folder_icon.pack(side=tkinter.RIGHT, expand=False, fill="y")
-   
-    window.mainloop()
+    upload_option(window, "Create folder")
 
 # def Receive():
 #     window = Toplevel(root)
@@ -122,6 +156,11 @@ Label(root, text="Folders", font=("TkSmallCaptionFont", 30, "bold"), bg="#f4fdfe
 
 #send icon
 send_image = Image.open("./assets/icons/send.png")
+
+#Update bg color of icon - Composite the original image on top of the new image
+new_send_img = Image.new("RGBA", send_image.size, "#f4fdfe")
+new_send_img.paste(send_image, (0, 0), send_image)
+
 resized_send = send_image.resize((100,100), Image.ANTIALIAS)
 
 send_image =ImageTk.PhotoImage(resized_send)
